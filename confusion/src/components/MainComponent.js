a99f2790381c09e2b42d29ca1482563510c8c3a0
yerle89 <martinyerle@gmail.com>
2022-10-14 15:06:51 +0200

    Assignment 2: React Router and Single Page Applications

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -8,6 +8,7 @@ import { LEADERS } from '../shared/leaders';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
@@ -67,6 +68,7 @@ class Main extends Component {
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
             <Route path='/menu/:dishId' component={DishWithId} />
             <Route exact path='/contactus' component={Contact} />
+            <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders}/> } />
             <Redirect to="/home" />
         </Switch>
         <Footer />
