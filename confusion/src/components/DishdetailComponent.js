ef40e678b776acfcf91143619cd32e207a4f719b
yerle89 <martinyerle@gmail.com>
2022-10-28 18:03:25 +0200

    Fetch Post Comment

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
   /*
   componentDidMount() {
@@ -41,7 +42,7 @@ import { Loading } from './LoadingComponent';
       //console.log('Current State is: ' + JSON.stringify(values));
       //alert('Current State is: ' + JSON.stringify(values));
       this.toggleModal();
-      this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+      this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
       // event.preventDefault();
   }
     render() {
@@ -130,7 +131,7 @@ import { Loading } from './LoadingComponent';
       return(
         <div className="col-12 col-md-5 m-1">
           <Card>
-            <CardImg top src={dish.image} alt={dish.name} />
+          <CardImg top src={baseUrl + dish.image} alt={dish.name} />
             <CardBody>
               <CardTitle>{dish.name}</CardTitle>
               <CardText>{dish.description}</CardText>
@@ -144,7 +145,7 @@ import { Loading } from './LoadingComponent';
       );
   }
 
-  function RenderComments({comments, addComment, dishId}) {
+  function RenderComments({comments, postComment, dishId}) {
     if (comments != null) 
     return ( 
       <div className="col-12 col-md-5 m-1">
@@ -160,7 +161,7 @@ import { Loading } from './LoadingComponent';
               })
             }
           </ul>
-          <CommentForm dishId={dishId} addComment={addComment} />
+          <CommentForm dishId={dishId} postComment={postComment} />
         </div>
     );
     else 
@@ -205,7 +206,7 @@ import { Loading } from './LoadingComponent';
           <div className="row">
             <RenderDish dish={props.dish} />
             <RenderComments comments={props.comments}
-                            addComment={props.addComment}
+                            postComment={props.postComment}
                             dishId={props.dish.id} />
           </div>
           </div>
--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -8,7 +8,7 @@ import About from './AboutComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
-import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { addComment, fetchDishes, fetchComments, fetchPromos, postComment } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
 
 const mapStateToProps = state => {
@@ -21,11 +21,11 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = dispatch => ({
-  
-  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+  postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
   fetchDishes: () => { dispatch(fetchDishes())},
-  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
-
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
+  fetchComments: () => dispatch(fetchComments()),
+  fetchPromos: () => dispatch(fetchPromos())
 });
 
 class Main extends Component {
@@ -39,8 +39,9 @@ class Main extends Component {
   }
 
   componentDidMount() {
-    console.log('MainComponent Comnponent componentDidMount invoked');
     this.props.fetchDishes();
+    this.props.fetchComments();
+    this.props.fetchPromos();
   }
 
   componentDidUpdate() {
@@ -56,11 +57,13 @@ class Main extends Component {
     const HomePage = () => {
       return(
         <Home 
-            dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
-            dishesLoading={this.props.dishes.isLoading}
-            dishesErrMess={this.props.dishes.errMess}
-            promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
-            leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+          dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+          dishesLoading={this.props.dishes.isLoading}
+          dishErrMess={this.props.dishes.errMess}
+          promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+          promoLoading={this.props.promotions.isLoading}
+          promoErrMess={this.props.promotions.errMess}
+          leader={this.props.leaders.filter((leader) => leader.featured)[0]}
         />
       );
     }
@@ -70,8 +73,9 @@ class Main extends Component {
         <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
           isLoading={this.props.dishes.isLoading}
           errMess={this.props.dishes.errMess}
-          comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
-          addComment={this.props.addComment}
+          comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+          commentsErrMess={this.props.comments.errMess}
+          postComment={this.props.postComment}
         />
       );
     };
